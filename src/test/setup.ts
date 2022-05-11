5839dcaabf326a4f407b0a240a7f63f27d62cbd8
Greg Tatum <tatum.creative@gmail.com>
2022-05-11 08:02:53 -0500

    Add test setups for jest

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,2 +1,21 @@
-// Currently nothing to do
-module.exports = () => {};
+// @ts-expect-error - Not sure why this is breaking.
+import fetchMock from 'fetch-mock-jest';
+import { Headers, Request, Response } from 'node-fetch';
+import { join } from 'path';
+
+require('dotenv').config({ path: join(__dirname, '../../.env.test') });
+
+beforeEach(function () {
+  global.fetch = fetchMock.sandbox();
+  (global as any).Headers = Headers;
+  (global as any).Request = Request;
+  (global as any).Response = Response;
+});
+
+afterEach(() => {
+  jest.resetAllMocks();
+  jest.restoreAllMocks();
+  jest.clearAllTimers();
+  jest.useRealTimers();
+  fetchMock.mockReset();
+});
