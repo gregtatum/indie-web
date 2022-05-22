eaac46050aa48e260736a2076f222d0349434884
Greg Tatum <tatum.creative@gmail.com>
2022-05-21 20:42:42 -0500

    Add a create chordpro button

--- src/components/ListFiles.tsx
+++ src/components/ListFiles.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import * as Redux from 'react-redux';
 import * as Router from 'react-router-dom';
 import { A, $ } from 'src';
+import { ensureExists, getStringProp } from 'src/utils';
 import { useRetainScroll } from './hooks';
 
 Router.useNavigationType;
 
 import './ListFiles.css';
+import { UnhandledCaseError } from '../utils';
 
 export function ListFiles() {
   const path = Redux.useSelector($.getPath);
@@ -121,6 +123,7 @@ export function ListFiles() {
                 </div>
               );
             })}
+            <CreateChordProButton path={path} />
           </div>
         </>
       );
@@ -144,4 +147,97 @@ export function ListFiles() {
         </div>
       );
   }
+}
+
+function CreateChordProButton(props: { path: string }) {
+  type Phase = 'not-editing' | 'editing' | 'submitting';
+  const dropbox = Redux.useSelector($.getDropbox);
+  const [phase, setPhase] = React.useState<Phase>('not-editing');
+  const input = React.useRef<HTMLInputElement | null>(null);
+  const navigate = Router.useNavigate();
+  const [error, setError] = React.useState<null | string>(null);
+
+  React.useEffect(() => {
+    switch (phase) {
+      case 'editing':
+        input.current?.focus();
+        input.current?.setSelectionRange(0, 0);
+        break;
+      case 'not-editing':
+      case 'submitting':
+        setError(null);
+        break;
+      default:
+        throw new UnhandledCaseError(phase, 'Phase');
+    }
+  }, [phase]);
+
+  function onSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    const inputEl = ensureExists(input.current, 'Input ref value');
+    let path = props.path;
+    if (path[path.length - 1] !== '/') {
+      path += '/';
+    }
+    path += inputEl.value;
+    const title = inputEl.value.replace(/\.chopro$/, '');
+    setPhase('submitting');
+    dropbox
+      .filesUpload({
+        path,
+        contents: `{title: ${title}}\n{subtitle: Unknown}`,
+        mode: {
+          '.tag': 'add',
+        },
+      })
+      .then(
+        (response) => {
+          navigate('file' + (response.result.path_display ?? path));
+        },
+        (error) => {
+          setError(
+            getStringProp(error, 'message') ?? 'There was a Dropbox API error',
+          );
+        },
+      );
+  }
+
+  if (phase === 'editing' || phase === 'submitting') {
+    const disabled = phase === 'submitting';
+    return (
+      <>
+        {error ? <div className="listFilesCreateError">{error}</div> : null}
+        <form className="listFilesCreateEditor" onSubmit={onSubmit}>
+          <input
+            type="text"
+            className="listFilesCreateEditorInput"
+            ref={input}
+            defaultValue=".chopro"
+            disabled={disabled}
+            onKeyDown={(event) => {
+              if (event.key === 'Escape') {
+                setPhase('not-editing');
+              }
+            }}
+          />
+          <input
+            type="submit"
+            value={phase === 'submitting' ? 'Submitting' : 'Create'}
+            className="button"
+            disabled={disabled}
+          />
+        </form>
+      </>
+    );
+  }
+
+  return (
+    <button
+      type="button"
+      className="button listFilesCreate"
+      onClick={() => setPhase('editing')}
+    >
+      Create ChordPro File
+    </button>
+  );
 }
