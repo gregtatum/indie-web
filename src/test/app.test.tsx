c6c28ef5c6c890d276e022ad31ceaa2fe19224b1
Greg Tatum <tatum.creative@gmail.com>
2022-06-17 09:02:24 -0500

    Fix primary app test

--- src/test/app.test.tsx
+++ src/test/app.test.tsx
@@ -1,17 +1,18 @@
 import { createStore } from 'src/store/create-store';
 import { App } from 'src/components/App';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import * as React from 'react';
 import { Provider } from 'react-redux';
 
 describe('app', () => {
-  it('can render', () => {
+  it('can render', async () => {
     const store = createStore();
     render(
       <Provider store={store as any}>
         <App />
       </Provider>,
     );
-    expect(screen.getByText(/React/)).toBeTruthy();
+
+    await waitFor(() => screen.getByText(/Browser Chords/));
   });
 });
