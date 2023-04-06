22c095b5a4277f7924b96ef232d1029f0bc813e9
Greg Tatum <tatum.creative@gmail.com>
2023-04-05 22:34:36 -0500

    Add waveform drawings

--- src/components/RenderedSong.tsx
+++ src/components/RenderedSong.tsx
@@ -295,6 +295,7 @@ function DropboxMedia({
     getEmptyMediaUrl(line.type),
   );
   const objectUrlRef = React.useRef(objectUrl);
+  const isEmpty = getEmptyMediaUrl(line.type) === objectUrl;
 
   // Set the object url to a ref so it can be used in event handlers.
   React.useEffect(() => {
@@ -349,10 +350,6 @@ function DropboxMedia({
 
   // Draw a wave form.
   React.useEffect(() => {
-    if (Math.random() < 10) {
-      // Temporarily disable since this is not working.
-      return;
-    }
     if (line.type !== 'audio') {
       return;
     }
@@ -364,52 +361,49 @@ function DropboxMedia({
     }
     const audio = mediaRef.current;
     const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    if (!context) {
-      return;
-    }
+    performance.mark('Chords: start effect');
 
     // The metadata must be loaded in order to draw the wave form.
-    audio.addEventListener('loadedmetadata', () => {
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    audio.addEventListener('loadedmetadata', async () => {
+      performance.mark('Chords: loadedmetadata');
       const audioContext = new AudioContext();
 
-      const width = canvas.width;
-      const height = canvas.height;
+      let { width, height } = canvas.getBoundingClientRect();
+      width *= devicePixelRatio;
+      height *= devicePixelRatio;
+      canvas.width = width;
+      canvas.height = height;
 
-      const sampleSize = 1000;
-      const bufferLength =
-        (audioContext.sampleRate * audio.duration) / sampleSize;
-      const dataArray = new Float32Array(bufferLength);
+      const ctx = ensureExists(canvas.getContext('2d'));
+      // Provie a filled in color.
+      ctx.fillStyle = '#ccc';
+      ctx.fillRect(0, 0, width, height);
 
-      const source = audioContext.createMediaElementSource(audio);
-      const analyzer = audioContext.createAnalyser();
+      performance.mark('Chords: Fetch array buffer');
+      const buffer = await fetch(objectUrl).then((r) => r.arrayBuffer());
+      performance.mark('Chords: Decode audio data');
+      const audioBuffer = await audioContext.decodeAudioData(buffer);
+      const { waveform, waveformBlurred, maxWaveHeight } = getWaveform(
+        audioBuffer,
+        width,
+      );
 
-      source.connect(analyzer);
-      source.connect(audioContext.destination);
+      // Clear out the color.
+      ctx.fillStyle = '#fff';
+      ctx.fillRect(0, 0, width, height);
 
-      analyzer.fftSize = 2048;
-
-      analyzer.getFloatTimeDomainData(dataArray);
-
-      context.fillStyle = 'rgb(255, 255, 255)';
-      context.fillRect(0, 0, width, height);
-      context.lineWidth = 2;
-      context.strokeStyle = 'rgb(0, 0, 0)';
-      context.beginPath();
-      console.log(`!!! dataArray`, dataArray);
-      const sliceWidth = width / bufferLength;
-      let x = 0;
-      for (let i = 0; i < bufferLength; i++) {
-        const v = Math.abs(dataArray[i]);
-        const y = (v * height) / 2;
-        if (i === 0) {
-          context.moveTo(x, y);
-        } else {
-          context.lineTo(x, y);
-        }
-        x += sliceWidth;
+      ctx.fillStyle = '#aaa';
+      for (let x = 0; x < waveform.length; x++) {
+        const y = (1 - waveform[x] / maxWaveHeight) * ctx.canvas.height;
+        ctx.fillRect(x, y, 1, ctx.canvas.height);
       }
-      context.stroke();
+
+      ctx.fillStyle = '#ce1ebb55';
+      for (let x = 0; x < waveformBlurred.length; x++) {
+        const y = (1 - waveformBlurred[x] / maxWaveHeight) * ctx.canvas.height;
+        ctx.fillRect(x, y, 1, ctx.canvas.height);
+      }
     });
   }, [line, objectUrl]);
 
@@ -450,7 +444,9 @@ function DropboxMedia({
 
   return (
     <>
-      <canvas style={{ width: '100%', height: '40px' }} ref={canvasRef} />
+      {isEmpty ? null : (
+        <canvas style={{ width: '100%', height: '100px' }} ref={canvasRef} />
+      )}
       <audio
         ref={mediaRef as AudioRef}
         controls
@@ -581,4 +577,79 @@ function getEmptyMediaUrl(type: 'video' | 'audio') {
   }
 
   return _emptyAudio;
+}
+
+function getWaveform(audioBuffer: AudioBuffer, size: number) {
+  performance.mark('Chords: getWaveform');
+  const leftChannel = audioBuffer.getChannelData(0);
+  const rightChannel = audioBuffer.getChannelData(1);
+
+  const waveform: number[] = [];
+  const window = Math.floor(leftChannel.length / size);
+  let sum = 0;
+  let maxWaveHeight = 0;
+  for (let i = 0; i < leftChannel.length; i++) {
+    sum += Math.abs(leftChannel[i]);
+    sum += Math.abs(rightChannel[i]);
+
+    if (i % window === window - 1) {
+      const value = sum / window;
+      waveform[Math.floor(i / window)] = value;
+      sum = 0;
+      maxWaveHeight = Math.max(maxWaveHeight, value);
+    }
+  }
+  return {
+    waveform: applyGaussianBlur(waveform, 5),
+    waveformBlurred: applyGaussianBlur(waveform, 20),
+    maxWaveHeight,
+  };
+}
+
+function applyGaussianBlur(numbers: number[], kernelSize: number): number[] {
+  const kernel = createGaussianKernel(kernelSize);
+  const result: number[] = [];
+
+  const halfKernelSize = Math.round(kernelSize / 2);
+  let sum = 0;
+  let numberIndex = 0;
+  let kernelIndex = 0;
+
+  while (numberIndex < numbers.length) {
+    let lookupIndex = numberIndex - halfKernelSize + kernelIndex;
+    // Keep the lookup in range.
+    lookupIndex = Math.max(lookupIndex, 0);
+    lookupIndex = Math.min(lookupIndex, numbers.length);
+
+    // The sum should never get over 1 here.
+    sum += kernel[kernelIndex] * numbers[lookupIndex];
+
+    kernelIndex++;
+
+    // The end of the kernel is reached, start on the next number.
+    if (kernelIndex === kernelSize) {
+      result[numberIndex] = sum;
+      numberIndex++;
+      sum = 0;
+      kernelIndex = 0;
+    }
+  }
+
+  return result;
+}
+
+function createGaussianKernel(n: number): number[] {
+  const sigma = n / 6;
+  const mean = (n - 1) / 2;
+  const kernel: number[] = [];
+
+  for (let i = 0; i < n; i++) {
+    kernel.push(
+      (1 / (Math.sqrt(2 * Math.PI) * sigma)) *
+        Math.exp(-((i - mean) ** 2) / (2 * sigma ** 2)),
+    );
+  }
+
+  const sum = kernel.reduce((a, b) => a + b, 0);
+  return kernel.map((x) => x / sum);
 }
