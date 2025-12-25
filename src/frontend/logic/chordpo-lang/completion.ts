a74045f30c404f2443f5255831f7ba306d0928ef
Greg Tatum <tatum.creative@gmail.com>
2025-12-25 12:32:25 -0600

    Fix completions a bit for ChordPro

--- src/frontend/logic/chordpo-lang/completion.ts
+++ src/frontend/logic/chordpo-lang/completion.ts
@@ -1,7 +1,10 @@
 import { EditorState } from '@codemirror/state';
 import { syntaxTree } from '@codemirror/language';
-import { CompletionContext, completeFromList } from '@codemirror/autocomplete';
-import { snippets } from './snippets';
+import {
+  CompletionContext,
+  completeFromList,
+  snippetCompletion,
+} from '@codemirror/autocomplete';
 
 // Get a list of all the chords used in the document
 export function chordsUsed(state: EditorState) {
@@ -31,14 +34,62 @@ export function completeChords(context: CompletionContext) {
   };
 }
 
-const completeSnippets = completeFromList(snippets);
+const valueDirectives = ['title', 'subtitle', 'artist', 'album', 'comment'];
+
+/**
+ * Complete the directives while inside of a { brace.
+ */
+const completeDirectivesInner = completeFromList(
+  valueDirectives.map((label) =>
+    snippetCompletion('' + label + ': ${}}', {
+      label,
+      type: 'directive',
+    }),
+  ),
+);
+
+/**
+ * Complete the directives whenever the first { is provided.
+ */
+const completeDirectivesOuter = completeFromList(
+  valueDirectives.map((label) =>
+    snippetCompletion('{' + label + ': ${}}', {
+      label: '{' + label,
+      displayLabel: label,
+      type: 'directive',
+    }),
+  ),
+);
 
 export function chordproCompletionSource(context: CompletionContext) {
   const node = syntaxTree(context.state).resolveInner(context.pos, -1);
 
+  if (node.type.name === '{') {
+    return completeDirectivesOuter(context);
+  }
+
+  if (node.type.name === 'DirectiveName') {
+    return completeDirectivesInner(context);
+  }
+
   if (node.name === '[' || node.name === 'Chord') {
     return completeChords(context);
-  } else {
-    return completeSnippets(context);
   }
+
+  return undefined;
 }
+
+// function completeJSDoc(context: CompletionContext) {
+//   let nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1)
+//   if (nodeBefore.name != "BlockComment" ||
+//       context.state.sliceDoc(nodeBefore.from, nodeBefore.from + 3) != "/**")
+//     return null
+//   let textBefore = context.state.sliceDoc(nodeBefore.from, context.pos)
+//   let tagBefore = /@\w*$/.exec(textBefore)
+//   if (!tagBefore && !context.explicit) return null
+//   return {
+//     from: tagBefore ? nodeBefore.from + tagBefore.index : context.pos,
+//     options: tagOptions,
+//     validFor: /^(@\w*)?$/
+//   }
+// }
