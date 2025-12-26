14b415e1420f175c0916d52e386493c6c1689bd8
Greg Tatum <tatum.creative@gmail.com>
2025-12-26 08:34:23 -0600

    Create a header for the text editor

--- src/frontend/components/TextArea.tsx
+++ src/frontend/components/TextArea.tsx
@@ -1,8 +1,12 @@
-import * as React from 'react';
-import { $$, T, A } from 'frontend';
-import { ensureExists, throttle1 } from 'frontend/utils';
-import * as Hooks from 'frontend/hooks';
-
+import {
+  syntaxHighlighting,
+  defaultHighlightStyle,
+} from '@codemirror/language';
+import { autocompletion, completionKeymap } from '@codemirror/autocomplete';
+import { history, defaultKeymap, historyKeymap } from '@codemirror/commands';
+import { highlightSelectionMatches, searchKeymap } from '@codemirror/search';
+import { EditorState, type EditorStateConfig } from '@codemirror/state';
+import { EditorView } from '@codemirror/view';
 import {
   drawSelection,
   dropCursor,
@@ -14,17 +18,33 @@ import {
   ViewUpdate,
   scrollPastEnd,
 } from '@codemirror/view';
-import {
-  syntaxHighlighting,
-  defaultHighlightStyle,
-} from '@codemirror/language';
-import { autocompletion, completionKeymap } from '@codemirror/autocomplete';
-import { history, defaultKeymap, historyKeymap } from '@codemirror/commands';
-import { highlightSelectionMatches, searchKeymap } from '@codemirror/search';
-import { EditorState, type EditorStateConfig } from '@codemirror/state';
-import { EditorView } from '@codemirror/view';
+import * as React from 'react';
+import { $$, T, A } from 'frontend';
+import { ensureExists, throttle1 } from 'frontend/utils';
+import * as Hooks from 'frontend/hooks';
 
 import './TextArea.css';
+
+export function TextAreaHeader(props: {
+  onHideEditor?: () => void;
+  children?: React.ReactNode;
+}) {
+  const dispatch = Hooks.useDispatch();
+  const onHideEditor =
+    props.onHideEditor ?? (() => dispatch(A.hideEditor(true)));
+
+  return (
+    <div className="textAreaHeader">
+      <button
+        className="textAreaHeaderDismiss"
+        type="button"
+        aria-label="Hide Editor"
+        onClick={onHideEditor}
+      />
+      {props.children}
+    </div>
+  );
+}
 
 export function TextArea(props: {
   path: string;
@@ -165,12 +185,7 @@ export function TextArea(props: {
 
   return (
     <div className="textArea" key={props.path}>
-      <button
-        className="textAreaDismiss"
-        type="button"
-        aria-label="Hide Editor"
-        onClick={() => dispatch(A.hideEditor(true))}
-      ></button>
+      <TextAreaHeader />
       <div
         className="textAreaMount"
         data-testid="textAreaMount"
