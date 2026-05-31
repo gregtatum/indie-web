1595a24b5dd3aceae3afda547977ee7078906f05
Greg Tatum <tatum.creative@gmail.com>
2026-05-31 08:17:33 -0500

    Add music filters tests

--- src/frontend/test/MusicFilters.test.tsx
+++ src/frontend/test/MusicFilters.test.tsx
@@ -4,6 +4,7 @@ import {
   render,
   screen,
   waitFor,
+  within,
 } from '@testing-library/react';
 import * as React from 'react';
 import { MemoryRouter, useLocation } from 'react-router-dom';
@@ -22,11 +23,11 @@ const FAKE_SERVER: T.FileStoreServer = {
 
 const TRACKS: T.TrackMetadata[] = [
   {
-    path: '/music/a.mp3',
-    title: 'Song A',
-    artist: 'Artist A',
-    album: 'Album A',
-    genre: 'Rock',
+    path: '/Indie/Andrew Bird/The Mysterious Production of Eggs/Sovay.mp3',
+    title: 'Sovay',
+    artist: 'Andrew Bird',
+    album: 'The Mysterious Production of Eggs',
+    genre: 'Indie',
     track: 1,
     duration: 180,
     size: 1024,
@@ -35,10 +36,10 @@ const TRACKS: T.TrackMetadata[] = [
     hasEmbeddedArt: false,
   },
   {
-    path: '/music/b.mp3',
-    title: 'Song B',
-    artist: 'Artist B',
-    album: 'Album B',
+    path: '/Jazz/Miles Davis/Kind of Blue/All Blues.mp3',
+    title: 'All Blues',
+    artist: 'Miles Davis',
+    album: 'Kind of Blue',
     genre: 'Jazz',
     track: 2,
     duration: 200,
@@ -89,26 +90,126 @@ function setup(search = '') {
   return { store, getLocation: () => currentLocation };
 }
 
-describe('music filter URL serialization', () => {
-  it('clicking a genre filter updates the URL', async () => {
+function getParams(search: string): URLSearchParams {
+  return new URLSearchParams(search);
+}
+
+describe('music URL serialization', () => {
+  it('updates the URL when clicking through filters and track selections', async () => {
     const { getLocation } = setup();
 
-    const rock = await screen.findByText('Rock');
-    await act(async () => {
-      fireEvent.click(rock);
-    });
+    const genrePanel = screen.getByRole('listbox', { name: 'genre' });
+    fireEvent.click(
+      await within(genrePanel).findByRole('option', { name: 'Indie' }),
+    );
 
     await waitFor(() => {
-      expect(getLocation().search).toContain('genre=Rock');
+      expect(getParams(getLocation().search).getAll('genre')).toEqual([
+        'Indie',
+      ]);
+    });
+
+    const artistPanel = screen.getByRole('listbox', { name: 'artist' });
+    fireEvent.click(
+      await within(artistPanel).findByRole('option', { name: 'Andrew Bird' }),
+    );
+
+    await waitFor(() => {
+      const params = getParams(getLocation().search);
+      expect(params.getAll('genre')).toEqual(['Indie']);
+      expect(params.getAll('artist')).toEqual(['Andrew Bird']);
+    });
+
+    const albumPanel = screen.getByRole('listbox', { name: 'album' });
+    fireEvent.click(
+      await within(albumPanel).findByRole('option', {
+        name: 'The Mysterious Production of Eggs',
+      }),
+    );
+
+    await waitFor(() => {
+      const params = getParams(getLocation().search);
+      expect(params.getAll('genre')).toEqual(['Indie']);
+      expect(params.getAll('artist')).toEqual(['Andrew Bird']);
+      expect(params.getAll('album')).toEqual([
+        'The Mysterious Production of Eggs',
+      ]);
+    });
+
+    const trackPanel = screen.getByRole('listbox', { name: 'Tracks' });
+    fireEvent.click(await within(trackPanel).findByText('Sovay'));
+
+    await waitFor(() => {
+      expect(getParams(getLocation().search).getAll('track')).toEqual([
+        '/Indie/Andrew Bird/The Mysterious Production of Eggs/Sovay.mp3',
+      ]);
     });
   });
 
-  it('loading a URL with filter params initializes the Redux state', async () => {
-    const { store } = setup('?genre=Rock');
+  it('cmd+clicking a second genre adds it to the URL', async () => {
+    const { getLocation } = setup();
+
+    fireEvent.click(await screen.findByText('Indie'));
+    fireEvent.click(screen.getByText('Jazz'), { metaKey: true });
+
+    await waitFor(() => {
+      expect(getParams(getLocation().search).getAll('genre')).toEqual([
+        'Indie',
+        'Jazz',
+      ]);
+    });
+  });
+});
+
+describe('music filter URL deserialization', () => {
+  it('uses the genre', async () => {
+    const { store } = setup('?genre=Indie');
 
     await waitFor(() => {
       expect($.getMusicPanelSelections(store.getState())).toEqual({
-        genre: ['Rock'],
+        genre: ['Indie'],
+      });
+    });
+  });
+
+  it('uses the artist', async () => {
+    const { store } = setup(`?artist=${encodeURIComponent('Andrew Bird')}`);
+
+    await waitFor(() => {
+      expect($.getMusicPanelSelections(store.getState())).toEqual({
+        artist: ['Andrew Bird'],
+      });
+    });
+  });
+
+  it('uses the album', async () => {
+    const { store } = setup(
+      `?album=${encodeURIComponent('The Mysterious Production of Eggs')}`,
+    );
+
+    await waitFor(() => {
+      expect($.getMusicPanelSelections(store.getState())).toEqual({
+        album: ['The Mysterious Production of Eggs'],
+      });
+    });
+  });
+
+  it('uses the track', async () => {
+    const { store } = setup(`?track=${encodeURIComponent('/music/a.mp3')}`);
+
+    await waitFor(() => {
+      expect($.getMusicSelectedTrackPaths(store.getState())).toEqual([
+        '/music/a.mp3',
+      ]);
+    });
+  });
+
+  it('supports multiple generes', async () => {
+    const { store } = setup('?genre=Indie&genre=Jazz');
+
+    await waitFor(() => {
+      expect($.getMusicPanelSelections(store.getState())).toEqual({
+        genre: ['Indie', 'Jazz'],
       });
     });
   });
